7821c19cb7325a0cf05d50e4d0e834b44f7662e1
Mike Larsson <mikelarssonftw@gmail.com>
2012-04-13 02:16:03 -0400

    adding in badgebadger sub-project

---
+++ static/badgebadger/js/badgebadger.js
@@ -0,0 +1,62 @@
+(function(jQuery){
+  var $ = jQuery;
+
+  $.extend({
+    toUrl: function(base){
+      if (!base)
+        throw new Error('toUrl needs a valid base, arguments: [' + Array.prototype.join.call(arguments, ', ') + ']');
+      var url = '';
+      if(!base.match(/:\/\//)){
+        url = 'http://';
+      }
+      url += base;
+      if(url.charAt(url.length-1) != '/') {
+        url += '/';
+      }
+      var pathElements = Array.prototype.slice.call(arguments, 1);
+      if(pathElements){
+        url += pathElements.join('/');
+      }
+      return url; 
+    },
+    Preview: function(el, fn){
+      function write(val){
+        $(el).text(val);
+      }
+      var that = {
+        element: el,
+        update: function(){
+          var val = fn(write);
+          if (val != undefined) {
+            write(val);
+          }
+        }
+      };
+      that.update();
+      return that;
+    },
+    Status: function(el, stats){
+      if($.isArray(stats)){
+        var statsMap = {};
+        stats.forEach(function(stat){
+          statsMap[stat] = stat;
+        });
+        stats = statsMap;
+      }
+      var values = [];
+      for (var key in stats){
+        values.push(stats[key]);
+      }
+      var that = {};
+      Object.keys(stats).forEach(function(key){
+        that[key] = function(){
+          $(el).removeClass(values.join(' '));
+          $(el).addClass(stats[key]);
+        };
+      });
+      return that;
+    },
+  
+  });
+
+})(jQuery);
---
+++ static/badgebadger/js/main.js
@@ -0,0 +1,127 @@
+
+$(function(){
+
+  function buildAssertion() {
+    var assertion = {         
+      "recipient": $('#email')[0].value,
+      "badge": {         
+        "version": $('#version')[0].value,
+        "name": $('#name')[0].value,
+        "image": $.toUrl(Config.hereFromBackpackUrl, 'img', 'badge.png'),
+        "description": $('#desc')[0].value,
+        "criteria": Config.hereFromBackpackUrl,
+        "issuer": {
+          // TODO: The 'origin' isn't checked right now,
+          // so we will take advantage of this to use an
+          // authoritative domain that's good for
+          // demo purposes.
+          "origin": "http://this.is.fake.org/",
+          "name": "BadgeBadger",
+          "org": "Experimental Badger Authority",
+          "contact": "hai2u@notreal.org"
+        }
+      }
+    };
+    return assertion;
+  }
+
+  var Config = {
+    init: function(){
+      $('#backpackUrl').change(function(evt){
+        Config.backpackUrl = $('#backpackUrl')[0].value;
+        var stat = $.Status(
+          $(evt.target).parent('.control-group'),
+          ['warning', 'error', 'success']
+        );
+        stat.warning();
+        var scriptUrl = $.toUrl(Config.backpackUrl, 'issuer.js');
+        $('#issuerScript').attr('src', scriptUrl); // issuer.js reads this to set its root
+        $.getScript(scriptUrl, function(){
+          stat.success();
+        }).fail(function(){
+          stat.error();
+        });
+      });
+      $('#backpackUrl').change();
+
+      $("#baseUrl").val($('<a href="./"></a>')[0].href);
+      $('#baseUrl').change(function(){
+        Config.hereFromBackpackUrl = $('#baseUrl')[0].value;
+      });
+      $('#baseUrl').change();
+    },
+  };
+  Config.init();
+
+  var assertionPreview = $.Preview('#assertion pre', function(){ 
+    return JSON.stringify(buildAssertion(), null, '  '); 
+  });
+
+  $('#metadata form').change(assertionPreview.update);
+  $('form #email').change(assertionPreview.update);
+
+  var digs = Digs();
+
+  var hostedAssertionsPrev = $.Preview('#hosted pre', function(write){ 
+    digs.list().success(function(data){
+      write(JSON.stringify(data, null, '  ')); 
+    });
+  });
+
+  $('#assertion form').submit(function(){
+    var stat = $.Status(
+      $('#submit', this),
+      {warning: 'btn-warning', success: 'btn-success', error: 'btn-error'}
+    );
+    stat.warning();
+    var id = $('#assertionId')[0].value;
+    var save;
+    if (id != undefined) {
+      save = digs.save(buildAssertion(), id);
+    }
+    else {
+      save = digs.save(buildAssertion());
+    }
+    save.success(function(){
+      stat.success();
+    }).error(function(){
+      stat.error();
+    }).done(function(){
+      hostedAssertionsPrev.update();
+    });
+    return false;
+  });
+
+  var callPreview = $.Preview('#preview', function(){ 
+    var ids = $('#assertions')[0].value;
+    var data = [];
+    if (ids) {
+      data = ids.split(/[\s,]+/).map(function(id){
+        // HACK - maybe Digs should provide it's url?
+        return $.toUrl(Config.hereFromBackpackUrl.replace(/\/badgebadger/, ''), 'dwellers', id);
+      });
+    }
+    return 'OpenBadges.issue('  
+      + JSON.stringify(data, null, '  ')
+      + ', callback);';
+  });
+  $('#assertions').change(callPreview.update);
+  $('#baseUrl').change(callPreview.update);
+
+  $('#issue form').submit(function(){
+    // DRY this out
+    var ids = $('#assertions')[0].value;
+    var data = [];
+    if (ids) {
+      data = ids.split(/[\s,]+/).map(function(id){
+        // HACK - maybe Digs should provide it's url?
+        return $.toUrl(Config.hereFromBackpackUrl.replace(/\/badgebadger/, ''), 'dwellers', id);
+      });
+    }
+    OpenBadges.issue(data, function(errors, successes){
+      $('#result').text(JSON.stringify({errors: errors, successes: successes}, null, '  '));
+    });
+    return false;
+  });
+
+});
