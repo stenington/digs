1da7a6b0d592b497c689695a02d8f4f4c7800dcd
Mike Larsson <mikelarssonftw@gmail.com>
2012-08-09 11:33:28 -0400

    allow CORS

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ function randomString(length) {
   return randomstring;
 }
 
+var allowCORS = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
 app.use(express.bodyParser());
+app.use(allowCORS);
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/dwellers', function(req, res){
